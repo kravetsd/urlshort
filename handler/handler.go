254b010ea1bb76089865d221a5a0ab734ceb5b35
kravetsd <kdykrg@gmail.com>
2023-02-01 00:17:31 +0300

    Added code for both the handlers and the main function. Also added the go.mod and go.sum files.

--- handler/handler.go
+++ handler/handler.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -12,7 +15,23 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return fallback.ServeHTTP
+	type pathUrl struct {
+		path string
+		url  string
+	}
+	var pathUrls []pathUrl
+	for path, url := range pathsToUrls {
+		pathUrls = append(pathUrls, pathUrl{path, url})
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, pathUrl := range pathUrls {
+			if r.URL.Path == pathUrl.path {
+				http.Redirect(w, r, pathUrl.url, http.StatusFound)
+				return
+			}
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -33,5 +52,19 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
-	return fallback.ServeHTTP, nil
+	type pathUrl struct {
+		Path string `yaml:"path"`
+		URL  string `yaml:"url"`
+	}
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yml, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := make(map[string]string)
+	for _, pathUrl := range pathUrls {
+		pathMap[pathUrl.Path] = pathUrl.URL
+	}
+	fmt.Println(pathMap)
+	return MapHandler(pathMap, fallback), err
 }
